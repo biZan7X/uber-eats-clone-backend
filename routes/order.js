7b29a316b5524b1e7e3eebde3d2499337b981770
Biswanath Tewari <biswanath@geekyants.com>
2021-10-19 18:38:48 +0530

    feat - Order model

--- routes/order.js
+++ routes/order.js
@@ -1,28 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const orders = [
-  {
-    name: "order1",
-  },
-  {
-    name: "order2",
-  },
-];
+const { Order, orderValidation } = require("../models/order");
 
-router.get("/", (req, res) => {
-  //TODO: fetch from the DB
-  res.status(200).send(orders);
+router.get("/", async (req, res) => {
+  const orders = await Order.find();
+  return res.status(200).send(orders);
 });
 
-router.post("/", (req, res) => {
-  //TODO: validation
-  const newOrder = {
-    name: req.body.name,
-  };
+router.post("/", async (req, res) => {
+  const { error } = orderValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-  orders.push(newOrder);
-  res.status(200).send(orders);
+  const newOrder = new Order({
+    bill: req.body.bill,
+    items: req.body.items,
+  });
+
+  const result = newOrder.save();
+
+  return res.status(200).send(result);
 });
 
 module.exports = router;
